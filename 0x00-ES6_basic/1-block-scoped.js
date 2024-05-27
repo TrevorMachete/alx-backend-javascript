e9d1725b135daaef753f747529d237ad7fa4cbc4
TrevorMachete <trevormachete@gmail.com>
2024-05-28 00:13:12 +0200

    prevent variable overwriting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
+  task = trueOrFalse ? true : task;
+  task2 = trueOrFalse ? false : task2;
 
   return [task, task2];
 }
