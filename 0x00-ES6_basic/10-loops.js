2bac4bce7cd1fc34300db60f92d636e8639e40ea
Macs Machete <trevormachete@gmail.com>
2024-05-28 01:25:56 +0200

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array.keys()) {
-    let value = array[idx];
+  for (const idx of array.keys()) {
+    const value = array[idx];
     array[idx] = appendString + value;
   }
 
