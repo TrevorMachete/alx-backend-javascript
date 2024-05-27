5e324fe78b95f770436f9eb4097b3cd51b443a65
Macs Machete <trevormachete@gmail.com>
2024-05-28 00:16:05 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task ||= true; // Assigns 'true' only if 'task' is falsy.
-    task2 ||= false; // Assigns 'false' only if 'task2' is falsy.
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
