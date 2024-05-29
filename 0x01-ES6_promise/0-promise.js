44ab502cddf56025e5568f0cc00f20894ec64010
Macs Machete <trevormachete@gmail.com>
2024-05-29 23:12:35 +0200

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,11 @@
-// 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-
+  return new Promise((resolve) => {
+    // Simulate an asynchronous operation, like fetching data from an API
+    setTimeout(() => {
+      // If the operation is successful, resolve the Promise
+      resolve('Response from API');
+      // If the operation fails, reject the Promise
+      // reject("Error: Could not get response from API");
+    }, 2000);
   });
 }
