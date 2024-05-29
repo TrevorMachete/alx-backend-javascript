bc9b08aaeecfb4c64192b24b52834311596347b2
Macs Machete <trevormachete@gmail.com>
2024-05-29 23:36:44 +0200

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,26 +1,13 @@
-// Importing uploadPhoto and createUser from utils.js
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils.js';
 
-// Function to handle profile signup
-function handleProfileSignup() {
-  // Calling uploadPhoto and createUser functions, which return promises
-  const uploadPromise = uploadPhoto('photo-profile-1');
-  const userPromise = createUser({
-    firstName: 'Guillaume',
-    lastName: 'Salva',
-  });
-
-  // Using Promise.all to collectively resolve all promises
-  Promise.all([uploadPromise, userPromise])
-    .then((values) => {
-      // values is an array of the resolved values, in the same order as the promises passed
-      console.log(`${values[1].body.firstName} ${values[1].body.lastName}`);
-    })
-    .catch(() => {
-      // In the event of an error, log 'Signup system offline' to the console
-      console.error('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    console.log(`${photo} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
 
-// Exporting handleProfileSignup function
 export default handleProfileSignup;
