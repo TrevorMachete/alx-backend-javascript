8e754df3fd23d91c93805f8b28ad6e866dfed471
TrevorMachete <trevormachete@gmail.com>
2024-05-29 23:05:51 +0200

    add files

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (const [idx, value] of array.entries()) {
+  for (const idx of array.keys()) {
+    const value = array[idx];
     newArray[idx] = appendString + value;
   }
+
   return newArray;
 }
