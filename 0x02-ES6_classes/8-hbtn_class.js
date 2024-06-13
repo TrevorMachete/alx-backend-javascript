87f88964c05adac1545bd4f4caaa699e1e10784a
Macs Machete <trevormachete@gmail.com>
2024-06-13 18:27:34 +0200

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,43 +1,12 @@
-// file: 8-holberton-class.js
-
-const size = Symbol('size');
-const location = Symbol('location');
-
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    this._size = size;
+    this._location = location;
   }
 
-  set size(value) {
-    if (typeof value !== 'number' || value < 0) {
-      throw new Error('size must be a non-negative number');
-    }
-    this[size] = value;
-  }
-
-  get size() {
-    return this[size];
-  }
-
-  set location(value) {
-    if (typeof value !== 'string') {
-      throw new Error('location must be a string');
-    }
-    this[location] = value;
-  }
-
-  get location() {
-    return this[location];
-  }
-
-  // Override the valueOf method to return the size when the class is cast into a Number
-  valueOf() {
-    return this.size;
-  }
-
-  // Override the toString method to return the location when the class is cast into a String
-  toString() {
-    return this.location;
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'string') return this._location;
+    if (hint === 'number') return this._size;
+    return 42;
   }
 }
