b273cf924150f79f8e84005a59ace1b00627beef
Macs Machete <trevormachete@gmail.com>
2024-06-13 18:24:26 +0200

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,11 +1,32 @@
-// file: main.js
-
-import Pricing from './4-pricing';
 import Currency from './3-currency';
 
-const currency = new Currency('USD', 'United States Dollar');
-const price = new Pricing(100, currency);
-console.log(price.displayFullPrice()); // Output: 100 United States Dollar (USD)
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-console.log(Pricing.convertPrice(100, 1.2)); // Output: 120
-// Add other code as needed
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
